20afc9f012372fb532e39cdc148bc87a442cc174
Eric Ma <ericmajinglong@gmail.com>
2014-06-23 14:58:43 -0400

    Created "environment"

--- environment.py
+++ environment.py
@@ -3,3 +3,8 @@ Author: Eric J. Ma
 Affiliation: Massachusetts Institute of Technology
 """
 
+class Environment(object):
+	"""docstring for Environment"""
+	def __init__(self):
+		super(Environment, self).__init__()
+		self.id = id
